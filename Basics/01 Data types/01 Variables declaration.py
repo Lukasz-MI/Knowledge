d5d80bdd94bdca5cbb64d2026d01b3e56c091d34
Lukasz <giela87@gmail.com>
2021-05-27 18:26:59 +0200

    Basics of Python

---
+++ Basics/01 Data types/01 Variables declaration.py
@@ -0,0 +1,18 @@
+# drobnostki dla początkujących
+
+number = 100
+
+number2 = 25
+
+result = number / number2
+
+print (result)
+
+"""
+
+The above was used to allow for
+commentary
+which is longer than
+one line
+
+"""
---
+++ Basics/01 Data types/02 type int, str, float, complex.py
@@ -0,0 +1,23 @@
+number = 100
+
+print (number)
+
+print (type (number)) # <class 'int'>
+
+numberInWords = "dwa"
+
+print (numberInWords)
+
+print (type (numberInWords)) # <class 'str'>
+
+x=3.14
+
+print (x)
+
+print (type(x)) # <class 'float'>
+
+complexNumber = 10 + 3j
+
+print (complexNumber)
+
+print (type(complexNumber)) # <class 'complex'>
